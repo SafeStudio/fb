6210c3d9bdf732068a4341486f0577396afd4421
Le Minh Tri <ansidev@gmail.com>
2017-08-23 05:00:48 +0700

    Remove function boot

--- src/FirebaseServiceProvider.php
+++ src/FirebaseServiceProvider.php
@@ -8,17 +8,6 @@ use Illuminate\Support\ServiceProvider;
 class FirebaseServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap firebase services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $loader = AliasLoader::getInstance();
-        $loader->alias('Firebase', 'SafeStudio\Firebase\Facades\FirebaseFacades');
-    }
-
-    /**
      * Register the application services.
      *
      * @return void
